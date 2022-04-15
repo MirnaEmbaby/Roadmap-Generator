dffc36cddc7dc65db1955149557973258c89d9dc
Mirna Embaby <73295764+MirnaEmbaby@users.noreply.github.com>
2022-04-15 15:10:55 +0200

    first

---
+++ lib/colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+Color darkGrey = const Color.fromRGBO(76, 70, 70, 1);
+Color grey = const Color.fromRGBO(90, 86, 85, 1);
+Color mint = const Color.fromRGBO(127, 205, 145, 1);
+Color white = const Color.fromRGBO(244, 234, 233, 1);
