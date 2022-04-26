2414321e11a926a15b033a0e2bb7b8ec27c381e0
Mirna Embaby <73295764+MirnaEmbaby@users.noreply.github.com>
2022-04-26 16:24:58 +0200

    some updates

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:roadmap_generator/colors.dart';
-import 'package:roadmap_generator/screens/main_screen.dart';
 
 class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
@@ -11,31 +10,30 @@ class Splash extends StatefulWidget {
 
 class _SplashState extends State<Splash> {
 
-  @override
-  void initState() {
-    super.initState();
-    _navigateToHome();
-  }
-
-  _navigateToHome()async{
-    await Future.delayed(const Duration(milliseconds: 2000,),(){});
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>MainScreen()));
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _navigateToHome();
+  // }
+  //
+  // _navigateToHome()async{
+  //   await Future.delayed(const Duration(milliseconds: 2000,),(){});
+  //   Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>MainScreen()));
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: white,
       body: Center(
         child: Container(
           color: white,
-          child: const Text(
-            "Splash",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+          child: Image.asset(
+        "assets/images/splash.png",
+        width: 150,
+        height: 150,
         ),
+      ),
       ),
     );
   }
